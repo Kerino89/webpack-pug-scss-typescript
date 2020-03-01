4b7cac335cc1deeb935cec9e31346bc2bb09b881
Utkin Anton <kerino89@gmail.com>
2020-03-01 15:48:39 +0300

    Update main.ts

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,7 +1,10 @@
 import "../scss/main.scss";
 
 function bootstrap(): void {
-  console.log("bootstrap", "color: red");
+  console.log(
+    "%c Webpack Pug Scss Typescript ",
+    "background: #000; color: #bada55; font-size: 18px;",
+  );
 }
 
 document.addEventListener("DOMContentLoaded", bootstrap);
